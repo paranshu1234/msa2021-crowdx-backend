9740c0ff0b1b5a7566201210f1a9f2fb0595afba
paranshu1234 <paranshu1234@gmail.com>
2021-09-04 12:26:04 +0530

    Added all queries

---
+++ backend/Extensions/ObjectFieldDescriptorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace backend.Extensions
+{
+    public static class ObjectFieldDescriptorExtensions
+    {
+        public static IObjectFieldDescriptor UseAppDbContext<TDbContext>(
+            this IObjectFieldDescriptor descriptor)
+            where TDbContext : DbContext
+        {
+            return descriptor.UseScopedService<TDbContext>(
+                create: s => s.GetRequiredService<IDbContextFactory<TDbContext>>().CreateDbContext(),
+                disposeAsync: (s, c) => c.DisposeAsync());
+        }
+    }
+}
---
+++ backend/Extensions/UseAppDbContextAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+using backend.Data;
+
+namespace backend.Extensions
+{
+    public class UseAppDbContextAttribute : ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(
+           IDescriptorContext context,
+           IObjectFieldDescriptor descriptor,
+           MemberInfo member)
+        {
+            descriptor.UseDbContext<AppDbContext>();
+        }
+    }
+}
